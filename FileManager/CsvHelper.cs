0545f5863709126f627b7ca81907b0e76ab880a8
GLOOR Gabriel <Gabriel.GLOOR@cpnv.ch>
2023-01-26 15:55:19 +0100

    CsvHelper_NominalCase_GetFileContent

--- FileManager/CsvHelper.cs
+++ FileManager/CsvHelper.cs
@@ -3,28 +3,42 @@
     public class CsvHelper : FileHelper
     {
         #region private attributs
-        //TODO Private attributs - 2pts
+        private char _separator;
         #endregion private attributs
 
         #region constructor
         public CsvHelper(string path, string fileName, char separator = ';') : base(path, fileName)
-        {
-            //TODO Constructor - 3pts
+        { 
+            _separator = separator;            
         }
         #endregion constructor
 
         #region public methods 
         public void ExtractFileContent()
         {
-            //TODO ExtractFileContent - 6pts
+            StreamReader streamReader = new StreamReader(_fullPath);
+            string line;
+            // Reads and stores lines from the file until eof.
+            while ((line = streamReader.ReadLine()) != null)
+            {
+                this.Lines.Add(line);
+            }
+            streamReader.Close();
+
+            if (this.Lines.Count == 0)
+            {
+                throw new EmptyFileException();
+            }
         }
         #endregion public methods
 
         #region private methods
         private bool IsCharSupported(char separator)
         {
-            //TODO IsCharSupported - 2pts
-            throw new NotImplementedException();
+            if(separator != '+')
+            {
+                throw new UnsupportedSeparatorException();
+            }
         }
         #endregion privates methods
 
